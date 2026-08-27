345ba9a438c2f63fd917dab199fd4bf2ea836119
KuaiSuMi <1987639310@qq.com>
2026-08-27 17:08:52 +0800

    chore: sync font preset metadata from main

---
+++ fonts/presets.js
@@ -0,0 +1,9 @@
+window.HANDWRITER_FONT_REMOTE_BASE = 'https://cdn.jsdelivr.net/gh/14790897/handwriting-web@main/ttf_files';
+
+window.HANDWRITER_FONT_PRESETS = Object.freeze({
+  yunyan: { name: '云烟体', file: '云烟体.ttf' },
+  huayang: { name: '华阳手写体', file: '华阳手写体.ttf' },
+  liguofu: { name: '李国夫手写体', file: '李国夫手写体.ttf' },
+  shenyunyingzi: { name: '神韵英子楷书', file: '神韵英子楷书.ttf' },
+  qingye: { name: '青叶手写体', file: '青叶手写体.ttf' }
+});
